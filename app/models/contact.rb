7d9987a4f9ecdc1b2809d7c952861c45f89a33cb
David Rouchy <drouchy@gmail.com>
2011-04-12 21:30:17 +0200

    implementing validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,6 +7,9 @@ class Contact < ActiveRecord::Base
   attr_reader :name
   attr_accessible :name
 
+  validates :email, :uniqueness => true, :format => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  validates :phone_number, :format => /00 \d{3} \d{6}/
+  
   def name
     "#{first_name} #{last_name}"
   end
--- test/factories/contact.rb
+++ test/factories/contact.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :contact do
     sequence :first_name   do |n| "User #{n}" end
     sequence :last_name    do |n| "last name #{n}" end
-    phone_number           do "00 #{random_integer_with_leading_zero(3)} #{random_integer_with_leading_zero(4)}" end
+    phone_number           do "00 #{random_integer_with_leading_zero(3)} #{random_integer_with_leading_zero(6)}" end
     email                  do "#{first_name}.#{last_name}@mydomain.com".downcase.gsub(/ /,'') end
     twitter_account        nil
     association :company, :factory => :company
