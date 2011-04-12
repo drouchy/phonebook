319c3586435e4d85ddb1e10f775db3d7147433c7
David ROUCHY <david.rouchy@alcatel-lucent.com>
2011-04-12 10:14:53 +0200

    linking company & contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,4 @@
 class Contact < ActiveRecord::Base
   has_attached_file :picture, :styles => { :medium => "140x140>", :thumb => "50x50" }
+  belongs_to :company
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110412075245) do
+ActiveRecord::Schema.define(:version => 20110412081206) do
+
+  create_table "companies", :force => true do |t|
+    t.string   "name"
+    t.text     "address"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "contacts", :force => true do |t|
     t.string   "first_name"
@@ -24,6 +31,7 @@ ActiveRecord::Schema.define(:version => 20110412075245) do
     t.string   "picture_content_type"
     t.integer  "picture_file_size"
     t.datetime "picture_updated_at"
+    t.integer  "company_id"
   end
 
 end
