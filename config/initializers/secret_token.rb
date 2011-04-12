079519a9f0bd0242470d83c5e02ce1eea4950ae7
David ROUCHY <david.rouchy@alcatel-lucent.com>
2011-04-12 08:38:48 +0200

    installing & configuring rspec & factory_girl

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phonebook::Application.config.secret_token = '1af67275c8a82edc728f24c164c7f35af206745adc6a0bc3cfaa3a2036eb73c4338971e0e18f6890b846b19df7303a638473a9d771d892731baca9ef5793f3bd'
