84827c2a7f779c453d8cb51d4cb51d3a2e8c754a
David Rouchy <drouchy@gmail.com>
2011-04-12 19:55:20 +0200

    few improvements

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,7 +3,10 @@ class Company < ActiveRecord::Base
   attr_accessible :contact_tokens
   attr_reader :contact_tokens
 
+  attr_accessible :name, :address
+  
   def contact_tokens=(ids)
     self.contact_ids = ids.split(',')
   end
+  
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,8 @@ class Contact < ActiveRecord::Base
   has_attached_file :picture, :styles => { :medium => "140x140>", :thumb => "50x50" }
   belongs_to :company
   
+  attr_accessible :first_name, :last_name, :email, :phone_number, :twitter_account, :picture
+  
   attr_reader :name
   attr_accessible :name
 
